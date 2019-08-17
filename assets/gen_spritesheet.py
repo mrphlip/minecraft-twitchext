b6b252769cb97af40b125ad7f139f9a42c4cfc34
phlip <git@mrphlip.com>
2019-08-17 14:05:44 +1000

    Use PIL instead of handrolled manip code

--- assets/gen_spritesheet.py
+++ assets/gen_spritesheet.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python3
 
-import png
+from PIL import Image
 import collections
 import json
 
 OUT_PATH = '../extension/public/'
-
-Image = collections.namedtuple('Image', ['width', 'height', 'image'])
-def read_image(filename):
-	width, height, data, metadata = png.Reader(filename=filename).asRGBA8()
-	data = [list(row) for row in data]
-	return Image(width, height, data)
-
-def write_image(image, filename):
-	with open(filename, "wb") as fp:
-		writer = png.Writer(width=image.width, height=image.height, alpha=True,
-			greyscale=False, bitdepth=8)
-		writer.write(fp, image.image)
 
 class Spritesheet:
 	def __init__(self, filename, iconwidth, iconheight, sheetwidth, sheetheight):
 		fullwidth = iconwidth * sheetwidth
 		fullheight = iconheight * sheetheight
 		if filename is None:
-			self.image = Image(fullwidth, fullheight, [[0] * fullwidth * 4 for i in range(fullheight)])
+			self.image = Image.new('RGBA', (fullwidth, fullheight), (0, 0, 0, 0))
 		else:
-			self.image = read_image(filename)
+			self.image = Image.open(filename)
 			assert self.image.width == iconwidth * sheetwidth
 			assert self.image.height == iconheight * sheetheight
+			assert self.image.mode == 'RGBA'
 		self.iconwidth = iconwidth
 		self.iconheight = iconheight
 		self.sheetwidth = sheetwidth
 		self.sheetheight = sheetheight
 		self.iconcount = sheetwidth * sheetheight
+
+	def save(self, *args, **kwargs):
+		return self.image.save(*args, **kwargs)
 
 	def tile_coords(self, pos):
 		if not (0 <= pos < self.iconcount):
@@ -44,23 +36,15 @@ class Spritesheet:
 
 	def tile(self, pos):
 		posx, posy = self.tile_coords(pos)
-		data = []
-		for row in self.image.image[posy:posy + self.iconheight]:
-			data.append(row[posx*4:(posx + self.iconwidth)*4])
-		return data
+		return self.image.crop((posx, posy, posx + self.iconwidth, posy + self.iconheight))
 
-	def set_tile(self, pos, data):
-		if not (0 <= pos < self.iconcount):
-			raise ValueError("pos out of bounds: %d" % pos)
-		if len(data) != self.iconheight:
-			raise ValueError("tile %d is wrong height" % pos)
-		if any(len(row) != self.iconwidth * 4 for row in data):
+	def set_tile(self, pos, im):
+		if im.width != self.iconwidth:
 			raise ValueError("tile %d is wrong width" % pos)
-		posy, posx = divmod(pos, self.sheetwidth)
-		posx *= self.iconwidth
-		posy *= self.iconheight
-		for y, row in enumerate(data):
-			self.image.image[y + posy][posx*4:(posx + self.iconwidth)*4] = row
+		if im.height != self.iconheight:
+			raise ValueError("tile %d is wrong height" % pos)
+		posx, posy = self.tile_coords(pos)
+		self.image.paste(im, (posx, posy, posx + self.iconwidth, posy + self.iconheight))
 
 with open("raw_advancement_data.json") as fp:
 	data = json.load(fp)
@@ -76,45 +60,28 @@ out_sprites = Spritesheet(None,
 	data['spritesheet']['tilewidth'], data['spritesheet']['tileheight'],
 	gen_width, gen_height)
 
-border_width = data['spritesheet']['tilewidth'] - data['spritesheet']['iconwidth']
-border_height = data['spritesheet']['tileheight'] - data['spritesheet']['iconheight']
-border_left = border_width // 2
-border_right = (border_width + 1) // 2
-border_top = border_height // 2
-border_bottom = (border_height + 1) // 2
+border_left = (data['spritesheet']['tilewidth'] - data['spritesheet']['iconwidth']) // 2
+border_top = (data['spritesheet']['tileheight'] - data['spritesheet']['iconheight']) // 2
+border_coords = (border_left, border_top,
+	border_left + data['spritesheet']['iconwidth'],
+	border_top + data['spritesheet']['iconheight'])
 
 backgrounds = {}
 for level, levelfn in [('advancement', 'plain'), ('goal', 'oval'), ('challenge', 'fancy')]:
 	for state, statefn in [(False, 'raw'), (True, 'worn')]:
-		backgrounds[level, state] = read_image("Advancement-%s-%s.png" % (levelfn, statefn))
+		backgrounds[level, state] = Image.open("Advancement-%s-%s.png" % (levelfn, statefn))
 		assert backgrounds[level, state].width == data['spritesheet']['tilewidth']
 		assert backgrounds[level, state].height == data['spritesheet']['tileheight']
+		assert backgrounds[level, state].mode == 'RGBA'
 
 def make_tile(pos, bg):
 	tile = raw_sprites.tile(pos)
-	border_tile = \
-		[[0] * (data['spritesheet']['tilewidth'] * 4) for i in range(border_top)] + \
-		[[0] * (border_left * 4) + row + [0] * (border_right * 4) for row in tile] + \
-		[[0] * (data['spritesheet']['tilewidth'] * 4) for i in range(border_bottom)]
+	border_tile = Image.new('RGBA',
+		(data['spritesheet']['tilewidth'], data['spritesheet']['tileheight']),
+		(0, 0, 0, 0))
+	border_tile.paste(tile, border_coords)
 	if bg is not None:
-		composed_tile = []
-		for bgrow, row in zip(backgrounds[bg].image, border_tile):
-			composed_row = []
-			for x in range(0, len(row), 4):
-				a = row[x + 3] / 255.0
-				bga = bgrow[x + 3] / 255.0
-				newa = a + bga - a * bga
-				if newa <= 0:
-					composed_row.extend([0, 0, 0, 0])
-				else:
-					composed_row.extend([
-						round((bgrow[x + 0] * bga * (1 - a) + row[x + 0] * a) / newa),
-						round((bgrow[x + 1] * bga * (1 - a) + row[x + 1] * a) / newa),
-						round((bgrow[x + 2] * bga * (1 - a) + row[x + 2] * a) / newa),
-						round(newa * 255),
-					])
-			composed_tile.append(composed_row)
-		return composed_tile
+		return Image.alpha_composite(backgrounds[bg], border_tile)
 	else:
 		return border_tile
 
@@ -132,7 +99,7 @@ for i, category in enumerate(data['categories']):
 	out_sprites.set_tile(i + 2 * len(data['advancements']), make_tile(category['icon'], None))
 	del category['icon']
 
-write_image(out_sprites.image, OUT_PATH+"images/spritesheet.png")
+out_sprites.save(OUT_PATH+"images/spritesheet.png")
 
 del data['spritesheet']
 with open(OUT_PATH+"advancement_data.json", "w") as fp:
