95bbe8ff910c73139501d5678a7c1fc783fc65c9
phlip <git@mrphlip.com>
2019-08-25 16:38:19 +1000

    Fix windows default MC path

--- client/constants.py
+++ client/constants.py
@@ -8,7 +8,7 @@ APP_DIRS = appdirs.AppDirs(APP_NAME, APP_AUTHOR, roaming=True)
 
 # see https://help.mojang.com/customer/portal/articles/1480874-where-are-minecraft-files-stored-
 if appdirs.system == 'win32':
-	DEF_MINECRAFT_DIR = appdirs.user_data_dir('.minecraft', roaming=True)
+	DEF_MINECRAFT_DIR = appdirs.user_data_dir('.minecraft', False, roaming=True)
 elif appdirs.system == 'darwin':
 	DEF_MINECRAFT_DIR = appdirs.user_data_dir('minecraft')
 else:
