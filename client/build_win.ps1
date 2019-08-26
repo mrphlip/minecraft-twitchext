07671b1fb034daa2a33d80848d66ee8e1e934f4d
phlip <git@mrphlip.com>
2019-08-26 17:50:21 +1000

    Delete zip first

--- client/build_win.ps1
+++ client/build_win.ps1
@@ -1,5 +1,8 @@
 $ErrorActionPreference = "Stop"
 python -m PyInstaller --onefile --name mc_twitchext --windowed --icon ..\assets\icon.ico --add-data "..\assets\icon.ico;." main.py
 cd dist
+If (Test-Path mc_twitchext.zip) {
+	Remove-Item mc_twitchext.zip
+}
 Compress-Archive mc_twitchext.exe mc_twitchext.zip
 Copy-Item mc_twitchext.zip ..\..\www\mc_twitchext.zip
