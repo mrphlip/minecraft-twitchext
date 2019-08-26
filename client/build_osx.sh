4f1d8a0dc6ea9dfd3a4b1799387a49143a2e39eb
phlip <git@mrphlip.com>
2019-08-26 17:56:53 +1000

    OSX also won't overwrite things

--- client/build_osx.sh
+++ client/build_osx.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
+rm -rf dist/mc_twitchext.app  # otherwise we'll get an overwrite warning
 python3 -m PyInstaller --onefile --name mc_twitchext --windowed --icon ../assets/icon.icns --add-data ../assets/icon.ico:. main.py
 # pyinstaller creates both dist/mc_twitchext (which doesn't work) and dist/mc_twitchext.app (which does)
 rm dist/mc_twitchext
 # create dmg image
-rm -rf build/dmg
+rm -rf build/dmg build/uncompressed.dmg dist/mc_twitchext.dmg
 mkdir build/dmg
 cp -a dist/mc_twitchext.app build/dmg
 hdiutil create build/uncompressed.dmg -ov -volname mc_twitchext -fs HFS+ -srcfolder build/dmg
