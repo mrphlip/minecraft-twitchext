b0945b419615432b1176d4134b2c3031915f0219
phlip <git@mrphlip.com>
2019-08-26 12:56:16 +1000

    Add ICNS for macOS

--- client/build.sh
+++ client/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -e
-python3 -m PyInstaller --onefile --name mc_twitchext --windowed --icon ../assets/icon.ico --add-data ../assets/icon.ico:. main.py
+python3 -m PyInstaller --onefile --name mc_twitchext --windowed --icon ../assets/icon.icns --add-data ../assets/icon.ico:. main.py
