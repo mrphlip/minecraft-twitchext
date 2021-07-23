895db0c9939cfcb670c55691599018633baf6436
phlip <git@mrphlip.com>
2021-07-24 00:05:01 +1000

    Fix path

--- client/buildsrc.sh
+++ client/buildsrc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 rm -f dist/mc_twitchext_src.zip
-zip dist/mc_twitchext_src.zip *.py */*.py Pipfile Pipfile.lock
-zip -g -j dist/mc_twitchext_src.zip  ../assets/icon.ico ../assets/data_versions.json ../assets/icon.icns
+zip dist/mc_twitchext_src.zip *.py */*.py
+zip -g -j dist/mc_twitchext_src.zip  ../assets/icon.ico ../assets/data_versions.json ../assets/icon.icns ../Pipfile ../Pipfile.lock
 cp dist/mc_twitchext_src.zip ../www/mc_twitchext_src.zip
